dbd093908263b436957ba8012f2ffa0e46f7827f
Jin Kwon <onacit@gmail.com>
2013-08-29 16:53:33 +0900

    javadoc updated

--- src/main/java/com/innods/Hello.java
+++ src/main/java/com/innods/Hello.java
@@ -19,13 +19,41 @@ package com.innods;
 
 
 /**
+ * A class whose main method prints out the words {@code hello, world} with a {@code line-feed(\n)}.
+ * <h3>Abstract</h3>
+ * <p> <p/>
+ * <p/>
+ * <blockquote>The only way to learn a new programming language is by writing programs in it. The first program to
+ * write is the same for all languages:<br/>Print the words<br/><code>hello, world</code></blockquote>
+ * The name of this Java file is derived from the original C file ('hello.c') and that's the reason why this class
+ * named as 'Hello' not 'HelloWorld'.
+ * <p/>
+ * <h3>Build and Run</h3>
+ * To build this project, simply execute following command in prompt.
+ * <pre>
+ * mvn clean install
+ * </pre>
+ * To run this class's main method, execute following command in prompt.
+ * <pre>
+ * mvn --quiet exec:java -Dexec.mainClass=com.innods.Hello
+ * </pre>
+ * Or execute the output jar directly like this. (Replace the {@code <version>} part with the actual value.)
+ * <pre>
+ * java -jar ./target/hello-world-&lt;version&gt;.jar (Unix)
+ * java -jar .\target\hello-world-&lt;version&gt;.jar (Windows)
+ * </pre>
+ *
  * @author Jin Kwon <onacit at gmail.com>
+ * @see <a href="http://www.pearsonhighered.com/educator/product/C-Programming-Language/9780131103627.page">C
+ *      Programming Language, 2/E (Peason Higher Ed)</a>
+ * @see <a href="http://en.wikipedia.org/wiki/The_c_programming_language">The C Programming Language (Wikipedia)</a>
  */
 public class Hello {
 
-
     /**
-     * @param args the arguments of this program
+     * Prints the words {@code hello, world} and a {@code line-feed(\n)} via system output.
+     *
+     * @param args the command line arguments. Note that this method doesn't use any of these arguments.
      */
     public static void main(final String[] args) {
 
